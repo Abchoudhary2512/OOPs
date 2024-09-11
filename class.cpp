e01b0376800db39cc6cfaa74bbf6887317b28571
ajay2512 <bhakarajay033@gmail.com>
2024-09-11 21:26:23 +0530

    classes code done

--- class.cpp
+++ class.cpp
@@ -1,6 +1,32 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+class Car
+{
+public:
+    string brand;
+    string model;
+    int year;
+
+    Car(string brand,string model,int year){
+        this->brand = brand;
+        this->model = model;
+        this->year = year;
+    }
+
+    void getInfo(){
+        cout<<brand<<" "<<year<<" "<<model<<endl;
+    }
+
+};
+
+int main()
+{
+    Car c1("toyota","model1",1221);
+    c1.getInfo();
+    return 0;
+
+
+
 
 }
