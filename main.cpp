689d7130f8f4794cdd4cc559dbd934aa4e24eecb
ajay2512 <bhakarajay033@gmail.com>
2024-09-11 14:17:23 +0530

    copy constructor done

--- main.cpp
+++ main.cpp
@@ -15,11 +15,18 @@ public:
         dept = "computer science";
     }
     // parametrized constructor
-    Teacher(string n,string d){
-        name = n;
-        dept = d;
+    Teacher(string name,string dept){
+        this->name = name;//parameter RHS MEIN rehta h
+        this->dept = dept;
     }
 
+    //copy constructor
+    Teacher(Teacher &orgObj){
+        cout<<"custome copy constructoir"<<endl;
+         //pass by reference
+    this->name = orgObj.name;
+    this->dept = orgObj.dept;
+    }
     
     string name;
     string dept;
@@ -45,7 +52,24 @@ class Student
 {
 public:
     string name;
-    int id;
+    float* cpiptr;
+
+    Student(string name,float cpi){
+        this->name = name;
+        cpiptr = new float;
+        *cpiptr = cpi;
+    }
+    void getInfo(){
+        cout<<"name is "<<name<<endl;
+        cout<<"cpi is "<<*cpiptr<<endl;
+    }
+
+    ~Student(){
+        cout<<"i delete everythin"<<endl;
+    }
+
+
+
 };
 
 class Account
@@ -61,15 +85,18 @@ public:
 
 int main()
 {
-    Teacher t1("ajay","CSE"); // constructor call ho jati hai
-    Teacher t2;
-    cout << t1.name << endl;
-    cout << t1.dept << endl;
+    // Teacher t1("ajay","CSE"); // constructor call ho jati hai
+    // Teacher t2(t1);
+    // cout << t2.name << endl;
+    // cout << t2.dept << endl;
 
-    t1.setLeaves(12);
-    cout << t1.getLeaves() << endl;
-    cout << t2.dept << endl;
-    
+    // t1.setLeaves(12);
+    // cout << t1.getLeaves() << endl;
+    // cout << t2.dept << endl;
+    Student s1("rahul bhai",8.7);
+    // s1.getInfo();
+    // Student s2(s1);
+    // s2.getInfo();
 
     // Teacher t2;
     // Teacher t3;
